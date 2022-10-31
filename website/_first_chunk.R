5b087284c5de9ccaad3db64cb1a0bf4e2a19efc5
Ben Whalley <ben.whalley@plymouth.ac.uk>
2022-10-31 12:09:44 +0000

    fix videos

--- website/_first_chunk.R
+++ website/_first_chunk.R
@@ -1,6 +1,9 @@
 library(tidyverse)
 library(pander)
 library(webex)
+library(whisker)
+library(vembedr)
+
 
 # devtools::install_github("benwhalley/psydata")
 library(psydata)
@@ -21,4 +24,18 @@ makermds <- function(video_data){
   fs::file_create(paste0("_content/", identifier, "_summary.rmd"))
 
   rstudioapi::navigateToFile(paste0("_content/", identifier, "_summary.rmd"))
+}
+
+
+video <- function(yt_id){ 
+  whisker.render('
+<div class="videowrapper">
+
+<div class="videowrapper-video">
+<iframe width="800" height="500" src="//www.youtube-nocookie.com/embed/{{yt_id}}?rel=0"
+frameborder="0" allow="autoplay; encrypted-media" allowfullscreen data-external="1"></iframe>
+
+</div>
+</div>
+')
 }
